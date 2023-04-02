fb583b64ca4399fbebeecdde421b89124166c06c
Jacob Lichty <jacobl@live.ca>
2023-04-01 23:11:08 -0400

    update engine

--- packages/engine/lib/abstracts/RenderingSystem.ts
+++ packages/engine/lib/abstracts/RenderingSystem.ts
@@ -1,8 +1,7 @@
-import { System, IComponentMaster } from '@plasmastrapi/ecs';
+import { System, IComponentMaster, IEntityMaster, ISystemMaster } from '@plasmastrapi/ecs';
 import IViewport from '../interfaces/IViewport';
 
 export default abstract class RenderingSystem extends System {
-  public once(): void {}
-
+  public once({}: { entities: IEntityMaster; components: IComponentMaster; systems: ISystemMaster; delta: number }): void {}
   public abstract draw({}: { viewport: IViewport<any>; components: IComponentMaster }): void;
 }
